098200045758304682d6b8ebad76a3171eb11454
Kelin Field <kfield111@hotmail.com>
2020-08-09 05:50:59 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,7 +11,7 @@ text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-text.match(/\b[A-Z]\w+[!.?]/) ? true : false
+text.match(/\b[A-Z]\w+[!.?",]\b/) ? true : false
 end
 
 def valid_phone_number?(phone)
